211608d1367975a3da5aade83f9523bf821fe6b4
Jacob Layton <jlayjlay3@gmail.com>
2022-06-04 19:25:05 -0700

    Update Cors pollicy

--- api/server.js
+++ api/server.js
@@ -1,41 +1,44 @@
-const express = require("express");
-const logger = require("morgan");
-const cors = require("cors");
-const bodyParser = require("body-parser");
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-const itemRoutes = require("./routes/itemRoutes");
-const orderRoutes = require("./routes/orderRoutes");
-const customerRoutes = require("./routes/customerRoutes");
-const emailRoutes = require("./routes/emailRoutes");
-const paymentRoutes = require("./routes/paymentRoutes");
+const itemRoutes = require('./routes/itemRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+const customerRoutes = require('./routes/customerRoutes');
+const emailRoutes = require('./routes/emailRoutes');
+const paymentRoutes = require('./routes/paymentRoutes');
 
 const server = express();
 
 // Logger
-server.use(logger("short"));
+server.use(logger('short'));
 
 // Cors
-server.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
-server.use("/payment/webhook", express.raw({ type: "*/*" }));
+const whitelist = [process.env.CLIENT_ORIGIN_URL];
+const corsOptions = {
+	origin: function (origin, cb) {
+		if (whitelist.indexOf(origin) !== -1) {
+			cb(null, true);
+		} else {
+			cb(new Error('Not allowed by CORS'));
+		}
+	},
+};
+server.use(cors(corsOptions));
+
+server.use('/payment/webhook', express.raw({ type: '*/*' }));
 server.use(express.json());
 
 // Routes
-server.use("/items", itemRoutes);
-server.use("/orders", orderRoutes);
-server.use("/customers", customerRoutes);
-server.use("/email", emailRoutes);
-server.use("/payment", paymentRoutes);
+server.use('/items', itemRoutes);
+server.use('/orders', orderRoutes);
+server.use('/customers', customerRoutes);
+server.use('/email', emailRoutes);
+server.use('/payment', paymentRoutes);
 
-server.use("/", (req, res) => {
-  res.send("Backend is running.");
+server.use('/', (req, res) => {
+	res.send('Backend is running.');
 });
 
 module.exports = server;
